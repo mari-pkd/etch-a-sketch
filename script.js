486c99f5e244d31b5644f8a83ebb54ffba0ed541
mariP <200~259964306+mari-pkd@users.noreply.github.com>
2026-02-26 03:40:25 +0300

    Add hover effect

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const canvas = document.querySelector(".canvas");
 
+//for editing canvas size in CSS:
+let canvasSide = 
+window.getComputedStyle(canvas, null).getPropertyValue("height");
+canvasSide = Number(canvasSide.slice(0, 3));
+
+let opacity = 10;
+
 function createCanvas(size) {
     let elementNumber = size ** 2;
-    let pixelSide = (560 / size) - 2 + "px";
+    let pixelSide = (canvasSide / size) - 2 + "px";
     for (let i = 0; i < elementNumber; i++) {
         let pixel = document.createElement("div");
         pixel.classList.add("pixel");
@@ -10,7 +17,15 @@ function createCanvas(size) {
         pixel.style.width = pixelSide;
         canvas.appendChild(pixel);
     }
-    console.log(pixelSide);
+
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mouseenter", () => {
+            pixel.style.background = "black";
+            pixel.style.opacity = opacity + "%";
+            opacity += 10;
+        });
+    })
 }
 
 createCanvas(16);
